463191318b2b44f713aed1e963172b67724a0ba9
Nataliia Kumeiko <nkumeiko@gmail.com>
2015-12-23 13:28:31 +0200

    Fix hound's finding

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,16 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 $:.unshift File.dirname(__FILE__)
 
-require 'rails_app/config/environment'
-require 'rails/test_help'
-require 'database_cleaner'
-require 'minitest/rails/capybara'
-require 'mongoid'
-require 'mongoid-history'
-require 'mongoid_search'
-require File.expand_path('../../lib/ants', __FILE__)
+require "rails_app/config/environment"
+require "rails/test_help"
+require "database_cleaner"
+require "minitest/rails/capybara"
+require "mongoid"
+require "mongoid-history"
+require "mongoid_search"
+require File.expand_path("../../lib/ants", __FILE__)
 
-
-Dir[ "#{File.dirname(__FILE__)}/rails_app/**/*.rb" ].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/rails_app/**/*.rb"].each { |f| require f }
 
 # Capybara
 Capybara.javascript_driver = :webkit
@@ -20,10 +19,12 @@ Capybara.javascript_driver = :webkit
 DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean_with(:truncation)
 
-class ActiveSupport::TestCase
-  def setup
-    Author.destroy_all
-    Book.destroy_all
-    DatabaseCleaner.clean_with(:truncation)
+class ActiveSupport
+  class TestCase
+    def setup
+      Author.destroy_all
+      Book.destroy_all
+      DatabaseCleaner.clean_with(:truncation)
+    end
   end
 end
